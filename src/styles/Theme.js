42b2efca431a66a1728a82f881a25fb722e88982
Bao Dang <85828374+weebao@users.noreply.github.com>
2023-02-04 17:03:58 -0500

    Fix mobile padding

--- src/styles/Theme.js
+++ src/styles/Theme.js
@@ -84,7 +84,7 @@ export const getThemedComponents = (mode) => ({
                   color: '#fff !important',
                   backgroundColor: 'transparent !important',
                   backfaceVisibility: 'hidden',
-                  padding: '24px !important',
+                  padding: '28px !important',
                   '@media (min-width: 600px)': {
                     padding: '32px !important',
                   },
